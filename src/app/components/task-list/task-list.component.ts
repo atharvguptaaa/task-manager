e6be47a92cbb771487740fd698036df8602ece06
atharvguptaaa <workinatharv@gmail.com>
2025-01-24 00:23:53 +0530

    c

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../models/task.model';
 import { TaskFormComponent } from "../task-form/task-form.component";
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskFormComponent],
+  imports: [TaskFormComponent,FormsModule, NgClass],
   templateUrl: './task-list.component.html',  
   styleUrl: './task-list.component.scss'
 })
